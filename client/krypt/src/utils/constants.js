1ca539f3a76c01f8657a3b74bedb5a1cd83de3cb
Idara-Cecilia Umana <idyumana.iu@gmail.com>
2023-02-25 00:02:48 +0100

    Added script to deploy smart contract and linked the react app to the smart contract

---
+++ client/krypt/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json';
+
+
+export const contractABI = abi.abi;
+export const contractAddress =  '0x9d5A899c4D39B31458222802786eb474044EA7Dd';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.17",
+// };
+
+// Https key 
+// https://eth-goerli.g.alchemy.com/v2/3kTI15R8qcanPLMnqfGQ1tvo8parENJ_
+
+require('@nomiclabs/hardhat-waffle');
+
+
 module.exports = {
-  solidity: "0.8.17",
-};
+    solidity: "0.8.17",
+    networks: {
+      goerli: {
+        url: 'https://eth-goerli.g.alchemy.com/v2/3kTI15R8qcanPLMnqfGQ1tvo8parENJ_',
+        accounts: [ "48b0886c611e815bf3b0a3f80ad307616af20468f2e9f54cdbdb21bead790fc7" ]
+      }
+    }
+  };
